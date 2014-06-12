40281a7b4c344ff60a597d9a8ce89649151cebdb
henkf <henk.freimuth@gmail.com>
2014-06-12 12:57:40 +0200

    initial project files

---
+++ desktop/src/de/rub/SVRVKVE/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package de.rub.SVRVKVE.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import de.rub.SVRVKVE.HerdSimulation;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new HerdSimulation(), config);
+	}
+}
