608e8481848b093e9ab7fb5de6166403e5830173
henkf <henk.freimuth@gmail.com>
2014-06-12 15:42:12 +0200

    Additional method stubs for sheep class

--- core/src/de/rub/SVRVKVE/Sheep.java
+++ core/src/de/rub/SVRVKVE/Sheep.java
@@ -2,15 +2,18 @@ package de.rub.SVRVKVE;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.GridPoint2;
 import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
 
 public class Sheep extends Rectangle {
 	
-	public static Texture image = new Texture(Gdx.files.internal("sheep3.png"));
+	public static final Texture image = new Texture(Gdx.files.internal("sheep3.png"));
+	private GridPoint2 destination;
+	private GridPoint2 position;
 	
 	public Sheep() {
 		// TODO Auto-generated constructor stub
-		System.out.println("OLOLOL");
 	}
 
 	public Sheep(Rectangle rect) {
@@ -21,6 +24,21 @@ public class Sheep extends Rectangle {
 	public Sheep(float x, float y, float width, float height) {
 		super(x, y, width, height);
 		// TODO Auto-generated constructor stub
+	}
+	
+	// Is being called by renderer to initiate movement. Result is saved in destination attribute
+	public void move() {
+		
+	}
+	
+	// Helper method to evaluate the next destination Point
+	private GridPoint2 evaluateMovement() {
+		return null;
+	}
+	
+	public Vector2 getHeading() {
+		// Return the directional vector between current position and destination
+		return null;
 	}
 
 }
