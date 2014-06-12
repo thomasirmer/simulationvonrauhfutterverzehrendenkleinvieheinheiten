f0540e68e66528b29be7c8adcf1c179a67aa44e5
henkf <henk.freimuth@gmail.com>
2014-06-12 13:36:20 +0200

    Added the sheep class

---
+++ core/src/de/rub/SVRVKVE/Sheep.java
@@ -0,0 +1,25 @@
+package de.rub.SVRVKVE;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Rectangle;
+
+public class Sheep extends Rectangle {
+	
+	public static Texture image = new Texture(Gdx.files.internal("sheep3.png"));
+	
+	public Sheep() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Sheep(Rectangle rect) {
+		super(rect);
+		// TODO Auto-generated constructor stub
+	}
+
+	public Sheep(float x, float y, float width, float height) {
+		super(x, y, width, height);
+		// TODO Auto-generated constructor stub
+	}
+
+}
