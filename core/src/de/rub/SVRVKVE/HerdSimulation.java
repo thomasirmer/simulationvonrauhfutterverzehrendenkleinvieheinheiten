28f66fc087b7d349256778b7cdd0a4940c6cb605
thomasirmer <thomasirmer@gmail.com>
2014-06-13 14:19:21 +0200

    - removed badlogic image - sheeps are now jerking on the screen

--- core/src/de/rub/SVRVKVE/HerdSimulation.java
+++ core/src/de/rub/SVRVKVE/HerdSimulation.java
@@ -1,27 +1,71 @@
 package de.rub.SVRVKVE;
 
+import java.util.Iterator;
+import java.util.Random;
+
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.TimeUtils;
 
 public class HerdSimulation extends ApplicationAdapter {
+	
+	public static final int WINDOW_X = 1440;
+	public static final int WINDOW_Y = 900;
+	
 	SpriteBatch batch;
-	Texture img;
+	
+	Array<Sheep> sheepHerd;
+	long lastUpdateTime = 0;
+	int sheepX 			= 50;
+	int sheepY 			= 50;
+	int numberOfSheeps 	= 256;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("sheep3.png");
+		
+		// initialize sheep herd
+		Random rand = new Random();
+		sheepHerd = new Array<Sheep>(numberOfSheeps);
+		for (int i = 0; i < numberOfSheeps; i++) {
+			sheepHerd.add(new Sheep(rand.nextInt(WINDOW_X), rand.nextInt(WINDOW_Y), sheepX, sheepY));
+		}
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(0, 1, 0, 1);
+		Gdx.gl.glClearColor(0.4f, 0.5f, 0.2f, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		
+		// move sheeps
+		if (TimeUtils.millis() - lastUpdateTime > 25) {
+			Iterator<Sheep> sheeperator = sheepHerd.iterator();
+			Random rand = new Random();
+			while (sheeperator.hasNext()) {
+				Sheep currentSheep = sheeperator.next();
+				currentSheep.x += rand.nextInt(3) - 1;
+				currentSheep.y += rand.nextInt(3) - 1;
+				if (currentSheep.x >= WINDOW_X - sheepX) currentSheep.x = WINDOW_X - sheepX;
+				if (currentSheep.x <= 0) currentSheep.x = 0;
+				if (currentSheep.y >= WINDOW_Y - sheepY) currentSheep.y = WINDOW_Y - sheepY;
+				if (currentSheep.y <= 0) currentSheep.y = 0;
+			}
+			lastUpdateTime = TimeUtils.millis();
+		}
+		
 		batch.begin();
-		batch.draw(img, 0, 0);
+		
+		// draw sheeps
+		Iterator<Sheep> sheeperator = sheepHerd.iterator();
+		while (sheeperator.hasNext()) {
+			Sheep currentSheep = sheeperator.next();
+			batch.draw(currentSheep.image, currentSheep.x, currentSheep.y, sheepX, sheepY);
+		}
+		
 		batch.end();
 	}
 }
--- desktop/src/de/rub/SVRVKVE/desktop/DesktopLauncher.java
+++ desktop/src/de/rub/SVRVKVE/desktop/DesktopLauncher.java
@@ -7,6 +7,8 @@ import de.rub.SVRVKVE.HerdSimulation;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width  = HerdSimulation.WINDOW_X;
+		config.height = HerdSimulation.WINDOW_Y;
 		new LwjglApplication(new HerdSimulation(), config);
 	}
 }
