8bf563c65ce4b405e9d11c35e1e958afb1924fdb
henkf <henk.freimuth@gmail.com>
2014-06-19 10:49:32 +0200

    first implementation of a mood-function with fuzzy

--- core/src/de/rub/SVRVKVE/Sheep.java
+++ core/src/de/rub/SVRVKVE/Sheep.java
@@ -1,44 +1,104 @@
 package de.rub.SVRVKVE;
 
+import java.io.IOException;
+
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.GridPoint2;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.utils.Array;
+
+import de.rub.fuzzy.Catalog;
+import de.rub.fuzzy.FuzzyInputException;
+import de.rub.fuzzy.FuzzyNoThenPartException;
 
 public class Sheep extends Rectangle {
+<<<<<<< Updated upstream:core/src/de/rub/SVRVKVE/Sheep.java
 	
 	public static final Texture image = new Texture(Gdx.files.internal("sheep3.png"));
+=======
+
+	public static final Texture image = new Texture(
+			Gdx.files.internal("sheepImage.png"));
+>>>>>>> Stashed changes:core/src/de/rub/SVRVKVE/animals/Sheep.java
 	private GridPoint2 destination;
-	private GridPoint2 position;
+	private Array<Sheep> herd;
+	private BitmapFont font;
 	
-	public Sheep() {
-		// TODO Auto-generated constructor stub
-	}
-
-	public Sheep(Rectangle rect) {
-		super(rect);
-		// TODO Auto-generated constructor stub
-	}
-
-	public Sheep(float x, float y, float width, float height) {
+	public Sheep(Array<Sheep> herd, float x, float y, float width, float height) {
 		super(x, y, width, height);
-		// TODO Auto-generated constructor stub
+		this.font = new BitmapFont();
+		this.font.setColor(Color.RED);
+		this.herd = herd;
 	}
-	
-	// Is being called by renderer to initiate movement. Result is saved in destination attribute
+
+	// Is being called by renderer to update movement. Result is saved in
+	// destination attribute
 	public void move() {
-		
+
 	}
-	
+
 	// Helper method to evaluate the next destination Point
 	private GridPoint2 evaluateMovement() {
 		return null;
 	}
-	
+
 	public Vector2 getHeading() {
-		// Return the directional vector between current position and destination
+		// Return the directional vector between current position and
+		// destination
 		return null;
 	}
 
+	private double measureDistance(Sheep target) {
+		return Math.sqrt(Math.pow(
+				Math.abs(target.x - this.x), 2)
+				+ Math.pow(Math.abs(target.x - this.x), 2));
+	}
+	
+	private Vector2 directionToSheep(Sheep target) {
+		return new Vector2((target.x-this.x), (target.y-this.y));
+	}
+	
+	private Array<Sheep> sheepInNearDistance() {
+		Array<Sheep> result = new Array<Sheep>();
+		for (Sheep neighbour : this.herd) {
+			if(measureDistance(neighbour)<25.0)
+				result.add(neighbour);
+		}
+		return result;
+	}
+	
+	private Array<Sheep> sheepInMediumDistance() {
+		Array<Sheep> result = new Array<Sheep>();
+		for (Sheep neighbour : this.herd) {
+			if(measureDistance(neighbour)<75.0 && measureDistance(neighbour)>=25)
+				result.add(neighbour);
+		}
+		return result;
+	}
+	
+	private Array<Sheep> sheepInFarDistance() {
+		Array<Sheep> result = new Array<Sheep>();
+		for (Sheep neighbour : this.herd) {
+			if(measureDistance(neighbour)<150.0 && measureDistance(neighbour)>=75)
+				result.add(neighbour);
+		}
+		return result;
+	}
+
+	public double evaluateExcitation() {
+		Catalog.set("close", sheepInNearDistance().size);
+		Catalog.set("inreach", sheepInMediumDistance().size);
+		Catalog.set("distant", sheepInFarDistance().size);
+		Catalog.evalAllRules();
+		return Catalog.get("Excitation");
+	}
+	
+	public BitmapFont getFont() {
+		return this.font;
+	}
 }
---
+++ core/src/de/rub/SVRVKVE/simulation/HerdSimulation.java
@@ -0,0 +1,186 @@
+package de.rub.SVRVKVE.simulation;
+
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.Random;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.GridPoint2;
+import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.TimeUtils;
+
+import de.rub.SVRVKVE.animals.Dog;
+import de.rub.SVRVKVE.animals.Sheep;
+import de.rub.SVRVKVE.input.InputHandler;
+import de.rub.fuzzy.Catalog;
+import de.rub.fuzzy.FuzzyInputException;
+import de.rub.fuzzy.FuzzyNoThenPartException;
+
+public class HerdSimulation extends ApplicationAdapter {
+	
+	// window size
+	public static final int WINDOW_X = 1280;
+	public static final int WINDOW_Y = 720;
+
+	// graphics
+	SpriteBatch batch;
+	OrthographicCamera camera;
+	Texture backgroundTexture;
+	BitmapFont font;
+	
+	// everything about the sheeps
+	Array<Sheep> sheepHerd;
+	Sound sheepSound;
+	int sheepWidth 		= 50;
+	int sheepHeigth 	= 50;
+	int numberOfSheeps 	= 64;
+	
+	// everything about the dog
+	Dog dog;
+	GridPoint2 dogStartPosition = new GridPoint2(50, 50);
+	int dogHeight = 100, dogWidth = 35;
+
+	// utilities
+	InputHandler inputHandler = new InputHandler();
+	Random rand = new Random();
+	long lastUpdateTime = 0;
+	
+	@Override
+	public void create() {
+		// set up graphics
+		batch = new SpriteBatch();
+		font = new BitmapFont();
+        font.setColor(Color.RED);
+
+		backgroundTexture = new Texture(Gdx.files.internal("grassTexture.jpg"));
+		
+		// set up input handler
+		Gdx.input.setInputProcessor(inputHandler);
+		
+		// set up camera
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, WINDOW_X, WINDOW_Y);
+
+		// initialize sheep herd
+		sheepSound = Gdx.audio.newSound(Gdx.files.internal("sheepSound.mp3"));
+		sheepHerd = new Array<Sheep>(numberOfSheeps);
+		for (int i = 0; i < numberOfSheeps; i++) {
+			sheepHerd.add(new Sheep(sheepHerd, rand.nextInt(WINDOW_X),
+						  rand.nextInt(WINDOW_Y),
+						  sheepWidth,
+						  sheepHeigth));
+		}
+
+		// initialize dog
+		dog = new Dog(dogStartPosition,dogHeight, dogWidth);
+		
+		// utilities
+		inputHandler.addInputListener(dog.getInputListener());
+		
+		// create Catalog
+		initFuzzy();
+	}
+
+	@Override
+	public void render() {
+		// clear screen
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		
+		// camera update
+		batch.setProjectionMatrix(camera.combined);
+		camera.update();
+
+		playSounds();
+		
+		moveSheeps();
+		
+		// begin drawing object to batch
+		batch.begin();
+		
+		// draw texture
+		batch.draw(backgroundTexture, 0, 0, WINDOW_X, WINDOW_Y);
+
+		drawSheeps();
+		
+		// display sheep's current mood
+		for (int i=0; i<sheepHerd.size; i++) {
+			Sheep s = sheepHerd.get(i);
+//			s.getFont().draw(batch, String.valueOf(s.evaluateSatisfaction()), s.x, s.y);
+			System.out.println("Sheep # "+i+"\'s excitation: "+sheepHerd.get(i).evaluateExcitation());
+		}
+
+		// draw dog
+		dog.render(batch);
+
+		batch.end();
+	}
+
+	private void drawSheeps() {
+
+		Iterator<Sheep> sheeperator = sheepHerd.iterator();
+
+		while (sheeperator.hasNext()) {
+			Sheep currentSheep = sheeperator.next();
+			batch.draw(Sheep.image, currentSheep.x, currentSheep.y, sheepWidth,
+					sheepHeigth);
+		}
+	}
+
+	private void moveSheeps() {
+		
+		if (TimeUtils.millis() - lastUpdateTime > 25) {
+			
+			Iterator<Sheep> sheeperator = sheepHerd.iterator();
+			
+			while (sheeperator.hasNext()) {
+				Sheep currentSheep = sheeperator.next();				
+				currentSheep.x += rand.nextInt(3) - 1;;
+				currentSheep.y += rand.nextInt(3) - 1;;
+				
+				if (currentSheep.x >= WINDOW_X - sheepWidth)
+					currentSheep.x = WINDOW_X - sheepWidth;
+				if (currentSheep.x <= 0)
+					currentSheep.x = 0;
+				if (currentSheep.y >= WINDOW_Y - sheepHeigth)
+					currentSheep.y = WINDOW_Y - sheepHeigth;
+				if (currentSheep.y <= 0)
+					currentSheep.y = 0;
+			}
+			lastUpdateTime = TimeUtils.millis();
+		}
+	}
+	
+	private void playSounds() {
+		if (rand.nextInt(256) == 1) {
+			sheepSound.play();
+		}
+	}
+	
+	@Override
+	public void dispose() {
+		batch.dispose();
+		super.dispose();
+	}
+	
+	private void initFuzzy() {
+		String workingDir = System.getProperty("user.dir");
+		   System.out.println("Current working directory : " + workingDir);
+		try {
+            Catalog.readFile("../core/assets/sheepRules.fzy");
+        } catch (FuzzyNoThenPartException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (FuzzyInputException e) {
+            e.printStackTrace();
+        }
+	}
+}
